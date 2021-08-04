0e7a4683c5296eb272dcdc4f6030ee4fe5842658
stkirk <shaanekirk@gmail.com>
2021-08-04 17:37:09 -0400

    fixed some styling

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,16 +4,18 @@ import ToDo from "./Todo";
 class ToDoList extends React.Component {
   render() {
     return (
-      <div className="todo-list">
-        {this.props.todos.map((todo) => {
-          return (
-            <ToDo
-              key={todo.id}
-              todo={todo}
-              toggleDone={this.props.toggleDone}
-            />
-          );
-        })}
+      <div className="todo-container">
+        <div className="todo-list">
+          {this.props.todos.map((todo) => {
+            return (
+              <ToDo
+                key={todo.id}
+                todo={todo}
+                toggleDone={this.props.toggleDone}
+              />
+            );
+          })}
+        </div>
 
         <button onClick={this.props.clearTodo} className="clear-btn">
           Clear Finished
